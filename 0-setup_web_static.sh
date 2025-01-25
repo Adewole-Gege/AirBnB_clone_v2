de012b2ec126a634cd5996b39cab2a068ea0f531
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-01-25 10:31:20 +0100

    Update 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,15 @@
 #!/usr/bin/env bash
-# Script to set up web servers for deployment of web_static
+# Set up the server for deployment of web_static
 
-# Install Nginx if not already installed
-sudo apt update
-sudo apt install -y nginx
+sudo apt-get update -y
+sudo apt-get install -y nginx
 
-# Create required directories
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "<html><body><h1>ALX</h1></body></html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create a simple HTML file in the test directory
-echo "<html>
-  <head>
-  </head>
-  <body>
-    ALX
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create a symbolic link to the test folder
-if [ -L /data/web_static/current ]; then
-  sudo rm /data/web_static/current
-fi
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of the /data/ directory to the ubuntu user and group
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update the Nginx configuration to serve the content
-nginx_conf="/etc/nginx/sites-available/default"
-sudo sed -i "/server_name _;/a \\ \n    location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }" $nginx_conf
-
-# Restart Nginx
-sudo service nginx restart
+sudo sed -i '/^\s*location \/ {/a \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo systemctl restart nginx
+exit 0
