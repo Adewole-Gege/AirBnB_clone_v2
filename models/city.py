715e68e6fdecbadbabe1dccdc1c738574cb00714
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-08 21:24:53 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,22 +1,24 @@
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from models.place import Place
 from os import getenv
-
 
 storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class City(BaseModel, Base):
+    """Representation of a city"""
     __tablename__ = 'cities'
 
     if storage_type == "db":
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
 
-        places = relationship('Place', backref='cities',
-                              cascade='all, delete-orphan')
+        places = relationship('Place', backref='cities', cascade='all, delete-orphan')
     else:
         name = ""
         state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize City instance"""
+        super().__init__(*args, **kwargs)
