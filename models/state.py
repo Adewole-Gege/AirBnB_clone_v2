44a07d7a27d6128931ff4a2ea169c59d535a0016
AL H. <alh@dr.com>
2025-02-08 23:05:20 +0100

    update

--- models/state.py
+++ models/state.py
@@ -1,12 +1,9 @@
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from os import getenv
 
-
 storage_type = getenv("HBNB_TYPE_STORAGE")
-
 
 class State(BaseModel, Base):
     """ State class """
@@ -17,22 +14,17 @@ class State(BaseModel, Base):
         name = Column(String(128), nullable=False)
 
         # Define relationship with City
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
+        cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
+
     else:
         name = ""
 
-    @property
-    def cities(self):
-        """Getter attribute for FileStorage relationship.
-        Returns the list of City instances with state_id
-        matching the current State id.
-        """
-        from models import storage
-        from models.city import City
-        city_list = []
-        for city in storage.all(City).values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
-
+        @property
+        def cities(self):
+            """Getter attribute for FileStorage relationship.
+            Returns the list of City instances with state_id
+            matching the current State id.
+            """
+            from models import storage
+            from models.city import City
+            return [city for city in storage.all(City).values() if city.state_id == self.id]
