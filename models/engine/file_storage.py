6d18d5f14ed1131d1f0da97cc74b7e5ae497dc34
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-11 16:24:04 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,17 +10,18 @@ import json
 
 class FileStorage:
     """
-    Serializes instances to a JSON file and deserializes them back to instances.
+    Serializes instances to a JSON file and deserializes
+    JSON file to instances.
     """
 
-    __file_path = 'file.json'
+    __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
         """
-        Returns a dictionary of all objects, or objects of a specific class if given.
+        Returns a dictionary of all objects, or objects of a specific class.
         """
-        if cls is not None:
+        if cls:
             return {
                 k: v for k, v in self.__objects.items() if isinstance(v, cls)
             }
@@ -30,17 +31,17 @@ class FileStorage:
         """
         Adds a new object to the storage dictionary.
         """
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            self.__objects[key] = obj
 
     def save(self):
         """
         Saves the current dictionary of objects to the JSON file.
         """
-        with open(self.__file_path, 'w', encoding='utf-8') as f:
+        with open(self.__file_path, "w", encoding="utf-8") as f:
             temp = {
-                key: obj.to_dict()
-                for key, obj in self.__objects.items()
+                key: obj.to_dict() for key, obj in self.__objects.items()
             }
             json.dump(temp, f)
 
@@ -50,22 +51,26 @@ class FileStorage:
         """
         from models.base_model import BaseModel
         from models.user import User
-        from models.place import Place
         from models.state import State
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
+        from models.place import Place
 
         classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State,   'City': City, 'Amenity': Amenity, 'Review': Review
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review,
+            "Place": Place
         }
-
         try:
-            with open(self.__file_path, 'r', encoding='utf-8') as f:
+            with open(self.__file_path, "r", encoding="utf-8") as f:
                 obj_dict = json.load(f)
             for key, value in obj_dict.items():
-                cls_name = value.get('__class__')
+                cls_name = value["__class__"]
                 if cls_name in classes:
                     self.__objects[key] = classes[cls_name](**value)
         except FileNotFoundError:
@@ -73,7 +78,7 @@ class FileStorage:
 
     def delete(self, obj=None):
         """
-        Deletes obj from __objects if it is inside.
+        Deletes obj from __objects if it exists.
         """
         if obj is not None:
             key = f"{obj.__class__.__name__}.{obj.id}"
@@ -82,6 +87,6 @@ class FileStorage:
 
     def close(self):
         """
-        Calls reload() method for deserializing the JSON file to objects.
+        Call reload() method for deserializing the JSON file to objects.
         """
         self.reload()
