30ec3a2c2bcfc0fdb54bc974598830e8d36bfcfb
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-02-11 16:27:04 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,34 +2,31 @@
 """
 FileStorage module for the HBNB project.
 
-Contains the FileStorage class that serializes instances to a JSON file
-and deserializes JSON file to instances.
+This module defines a class to manage file storage in JSON format.
 """
+
 import json
 
 
 class FileStorage:
     """
-    Serializes instances to a JSON file and deserializes
-    JSON file to instances.
+    Serializes instances to a JSON file and deserializes JSON file to instances.
     """
-
     __file_path = "file.json"
     __objects = {}
 
     def all(self, cls=None):
         """
-        Returns a dictionary of all objects, or objects of a specific class.
+        Returns the dictionary __objects.
+        If cls is provided, returns objects of that type.
         """
         if cls:
-            return {
-                k: v for k, v in self.__objects.items() if isinstance(v, cls)
-            }
+            return {k: v for k, v in self.__objects.items() if isinstance(v, cls)}
         return self.__objects
 
     def new(self, obj):
         """
-        Adds a new object to the storage dictionary.
+        Sets in __objects the obj with key <obj class name>.id.
         """
         if obj:
             key = f"{obj.__class__.__name__}.{obj.id}"
@@ -37,17 +34,15 @@ class FileStorage:
 
     def save(self):
         """
-        Saves the current dictionary of objects to the JSON file.
+        Serializes __objects to the JSON file (path: __file_path).
         """
         with open(self.__file_path, "w", encoding="utf-8") as f:
-            temp = {
-                key: obj.to_dict() for key, obj in self.__objects.items()
-            }
+            temp = {key: obj.to_dict() for key, obj in self.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
         """
-        Loads data from the JSON file into __objects.
+        Deserializes the JSON file to __objects (only if the JSON file exists).
         """
         from models.base_model import BaseModel
         from models.user import User
@@ -87,6 +82,6 @@ class FileStorage:
 
     def close(self):
         """
-        Call reload() method for deserializing the JSON file to objects.
+        Calls reload() method for deserializing the JSON file to objects.
         """
         self.reload()
