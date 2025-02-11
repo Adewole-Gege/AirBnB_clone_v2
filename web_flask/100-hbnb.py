cf872336168b0236fd12efa6f24b2fbf235c6389
AL H. <alh@dr.com>
2025-02-11 16:48:11 +0100

    update

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -5,18 +5,25 @@ from models import storage
 
 app = Flask(__name__)
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays an HTML page with filters for states, cities, amenities, and places."""
     states = sorted(storage.all("State").values(), key=lambda s: s.name)
     amenities = sorted(storage.all("Amenity").values(), key=lambda a: a.name)
     places = sorted(storage.all("Place").values(), key=lambda p: p.name)
-    return render_template("100-hbnb.html", states=states, amenities=amenities, places=places)
+    return render_template(
+        "100-hbnb.html",
+        states=states,
+        amenities=amenities,
+        places=places)
+
 
 @app.teardown_appcontext
 def teardown(exception):
     """Closes the database session after each request."""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
