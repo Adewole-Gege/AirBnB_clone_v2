fddd11dd926dd389807b1e804d88a96aeb38d4fd
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-03-03 20:56:33 +0100

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """
 Starts a Flask web application that listens on 0.0.0.0:5000.
-It defines a single route "/" that returns "Hello HBNB!".
+It defines a single route "/airbnb-onepage/" that returns "Hello HBNB!".
 """
 
 from flask import Flask
 
 app = Flask(__name__)
 
-
-@app.route("/", strict_slashes=False)
-def hello():
-    """Route handler that returns 'Hello HBNB!' when the root URL is accessed."""
+@app.route("/airbnb-onepage/", strict_slashes=False)
+def hello_hbnb():
+    """Route handler that returns 'Hello HBNB!' when accessed."""
     return "Hello HBNB!"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
